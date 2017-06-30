622bade75bcc5764729a404a523c2d303a568882
katiepetersonn <katie.peterson19@gmail.com>
2017-07-01 09:33:58 +1000

    files connected and initial CSS

---
+++ src/TrafficLight.js
@@ -0,0 +1,20 @@
+// You are required to provide the code for an application that simulates a set of traffic lights at an intersection.
+// The traffic lights are designated (N, S) and (E, W) like a compass.
+// You're not required to optimize, just focus on a functional approach to requirements.
+// You should provide unit tests for your solution.
+
+// REQUIREMENTS
+
+// The lights change automatically every 5 minutes.
+
+// When switching from green to red, the yellow light must be displayed for 30 seconds prior to it
+
+// switching to red and the opposite direction switching to green from red.
+
+// Provide the output for the light changes which occur during the period between 9am and 9:30am.
+
+//loop to change lights
+
+//change lights
+
+console.log("Hello world");
